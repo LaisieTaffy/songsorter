efbcf4e5bc9ad96d8acc7ecdd329de6f29c7a52e
LaisieTaffy <36828175+LaisieTaffy@users.noreply.github.com>
2020-02-27 00:20:52 +0100

    Changed some colors

--- js/op.js
+++ js/op.js
@@ -1,20 +1,20 @@
 ﻿function OpenWnd() {
-  var oWindow = window.open("", ""); 
+  var oWindow = window.open("", "");
   with (oWindow.document) {
     write("<html>");
     write("<head>");
     write("<title>JKT48 Sort: Raw Text Results<\/title>");
     write("<style>");
-		write("body { background-color: #FFD1DC; color: #000; font-family: \"Helvetica Neue\",Helvetica,Arial,sans-serif; font-size: 15px; }");
+		write("body { background-color: #171717; color: ghostwhite; font-family: \"Helvetica Neue\",Helvetica,Arial,sans-serif; font-size: 15px; }");
 		write("<\/style>");
     write("<\/head>");
     write("<body>");
     write(csort5);
     write("<hr>");
     write("<input type='button' value='Close' onclick='window.close()'>");
-    write("<hr>");		
+    write("<hr>");
 		write("<\/body>");
     write("<\/html>");
-    close(); 
+    close();
   }
 }
